ae4b16fdbc809653364ef112836f9adeaf9a6bd1
javafullstackD <javafullstackdeveloper705@gmail.com>
2022-06-28 23:30:59 +0200

    few updates on the project

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -1,13 +1,36 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Phumlani");
+		employee1.setLastName("Mlambo");
+		employee1.setEmailId("phumlanijack25@gmail.com");
+
+		Employee employee2 = new Employee();
+		employee2.setFirstName("Philani");
+		employee2.setLastName("Mlambo");
+		employee2.setEmailId("philanijackson25@gmail.com");
+
+		employeeRepository.save(employee1);
+		employeeRepository.save(employee2);
+
+
+	}
 }
---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    //build a create method rest api for employees
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return  employeeRepository.save(employee);
+    }
+}
