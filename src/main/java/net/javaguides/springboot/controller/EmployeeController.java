353118d1637efc871a6aa43b30116a523c5f181a
javafullstackD <javafullstackdeveloper705@gmail.com>
2022-06-30 16:33:34 +0200

    updated the controller

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package net.javaguides.springboot.controller;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,4 +25,12 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee){
         return  employeeRepository.save(employee);
     }
+    //get employee by id method
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee id not exist"+id));
+        return ResponseEntity.ok(employee);
+    }
 }
+
